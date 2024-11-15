d094c91dac1c094170168b1977573c8f81eef223
Jace Galloway <jace.galloway@gmail.com>
2024-11-15 14:28:58 -0500

    edited footer and added bootstrap

--- src/main.jsx
+++ src/main.jsx
@@ -8,12 +8,13 @@ import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function MainApp() {
 	return (
 		<>
 			<Header />
-			<main>
+			<main className='container'>
 				<Routes>
 					<Route path='/' element={<About />} />
 					<Route path='/portfolio' element={<Portfolio />} />
