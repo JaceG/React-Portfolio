3fb3a490675b62c96237c8c5af6d8b41318aae60
Jace Galloway <jace.galloway@gmail.com>
2024-11-20 15:44:29 -0500

    updated resume page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Title from '../components/Title';
+import '../style.css';
 
 function Resume() {
 	return (
@@ -10,12 +11,22 @@ function Resume() {
 			</a>
 
 			<section className='resume-section'>
-				<h2 className='section-title'>Summary</h2>
+				<h2 className='section-title'>Jace Galloway</h2>
 				<p className='summary-text'>
-					A concise summary of your skills, experience, and career
-					goals. For example: "Recent graduate from The Ohio State
-					University Full Stack Development Bootcamp with a focus on
-					React and expertise in communication and problem-solving."
+					I am looking to build a career in Full Stack React Web
+					Development. My skills for this field are listed in detail
+					at the bottom of my resume. Over the years, I have gained
+					diverse experience in various roles.
+				</p>
+				<p>
+					I started as a Quality Assurance Analyst, ensuring the
+					quality of the MLB The Show video game franchise. I then
+					moved into a Marketing role, where I honed my skills in
+					marketing strategies and implementation. My experience as a
+					Driver and an Insurance Broker has enhanced my communication
+					and problem-solving skills. I am now eager to leverage my
+					comprehensive skill set in a Full Stack React Web
+					Development role.
 				</p>
 			</section>
 
@@ -23,16 +34,43 @@ function Resume() {
 				<h2 className='section-title'>Work Experience</h2>
 				<ul className='work-experience-list'>
 					<li className='work-item'>
-						<h3 className='job-title'>Job Title</h3>
+						<h3 className='job-title'>Insurance Broker</h3>
 						<p>
-							<strong>Company Name</strong> – <em>City, State</em>
+							<strong>Independant</strong> –{' '}
+							<em>Tampa, Florida</em>
 						</p>
-						<p>MM/YYYY – MM/YYYY</p>
-						<ul className='job-responsibilities'>
-							<li>Responsibility or achievement 1</li>
-							<li>Responsibility or achievement 2</li>
-							<li>Responsibility or achievement 3</li>
-						</ul>
+					</li>
+					<li className='work-item'>
+						<h3 className='job-title'>Driver</h3>
+						<p>
+							<strong>Uber & Lyft</strong> –{' '}
+							<em>San Diego, California - </em>
+							<em>Tampa, Florida - </em>
+							<em>Columbus, Ohio</em>
+						</p>
+					</li>
+					<li className='work-item'>
+						<h3 className='job-title'>Marketing Manager</h3>
+						<p>
+							<strong>ADventures</strong> –{' '}
+							<em>San Diego, California</em>
+						</p>
+					</li>
+					<li className='work-item'>
+						<h3 className='job-title'>
+							Marketing & Data Entry Specialist
+						</h3>
+						<p>
+							<strong>Fix Your Funnel</strong> –{' '}
+							<em>San Diego, California</em>
+						</p>
+					</li>
+					<li className='work-item'>
+						<h3 className='job-title'>Quality Assurance Analyst</h3>
+						<p>
+							<strong>Sony</strong> –{' '}
+							<em>San Diego, California</em>
+						</p>
 					</li>
 				</ul>
 			</section>
@@ -41,36 +79,69 @@ function Resume() {
 				<h2 className='section-title'>Education</h2>
 				<ul className='education-list'>
 					<li className='education-item'>
-						<h3 className='degree'>Degree</h3>
+						<h3 className='degree'>
+							Full Stack React Web Development Bootcamp
+							Certification
+						</h3>
 						<p>
-							<strong>School Name</strong> – <em>City, State</em>
+							<strong>The Ohio State University</strong> –{' '}
+							<em>Columbus, Ohio</em>
 						</p>
-						<p>MM/YYYY – MM/YYYY</p>
+					</li>
+					<li className='education-item'>
+						<h3 className='degree'>
+							Digital Marketing Certification
+						</h3>
+						<p>
+							<strong>Thinkful</strong> – <em>Online</em>
+						</p>
 					</li>
 				</ul>
 			</section>
 
 			<section className='resume-section'>
 				<h2 className='section-title'>Skills</h2>
+				<h3 className='small-title'>Frontend Development Skills:</h3>
 				<ul className='skills-list'>
-					<li>Skill 1</li>
-					<li>Skill 2</li>
-					<li>Skill 3</li>
-					<li>Skill 4</li>
+					<li>HTML</li>
+					<li>CSS</li>
+					<li>Javascript</li>
+					<li>React</li>
+					<li>Typescript</li>
+					<li>Wireframing</li>
 				</ul>
-			</section>
 
-			<section className='resume-section'>
-				<h2 className='section-title'>Projects</h2>
-				<ul className='projects-list'>
-					<li className='project-item'>
-						<h3 className='project-name'>Project Name</h3>
-						<p>
-							<strong>Technologies Used:</strong> Technology 1,
-							Technology 2
-						</p>
-						<p>Description of the project.</p>
-					</li>
+				<h3 className='small-title'>Backend Development Skills:</h3>
+				<ul className='skills-list'>
+					<li>Node.js</li>
+					<li>Object-Oriented Programming</li>
+					<li>Servers</li>
+					<li>APIs</li>
+					<li>Python</li>
+				</ul>
+
+				<h3 className='small-title'>Database Skills:</h3>
+				<ul className='skills-list'>
+					<li>PostgreSQL</li>
+					<li>MongoDB</li>
+					<li>MERN</li>
+				</ul>
+
+				<h3 className='small-title'>Tools and Technologies:</h3>
+				<ul className='skills-list'>
+					<li>Git</li>
+					<li>Postman</li>
+					<li>CI/CD</li>
+					<li>Deployment</li>
+				</ul>
+
+				<h3 className='small-title'>Other Skills:</h3>
+				<ul className='skills-list'>
+					<li>Chat GPT</li>
+					<li>Copilot</li>
+					<li>Testing</li>
+					<li>Refactoring</li>
+					<li>Prompt Engineering</li>
 				</ul>
 			</section>
 		</section>
