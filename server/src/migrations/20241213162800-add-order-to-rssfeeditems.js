29c1c550e36fee5309f4732b54aa87e96914b5b1
Jace Galloway <jace.galloway@gmail.com>
2024-12-13 18:34:21 -0500

    Convert migration to CommonJS syntax

--- server/src/migrations/20241213162800-add-order-to-rssfeeditems.js
+++ server/src/migrations/20241213162800-add-order-to-rssfeeditems.js
@@ -1,15 +1,32 @@
 'use strict';
 
 module.exports = {
-	up: async (queryInterface, Sequelize) => {
-		await queryInterface.addColumn('RssFeedItems', 'order', {
-			type: Sequelize.INTEGER,
-			allowNull: false,
-			defaultValue: 0,
-		});
+	async up(queryInterface, Sequelize) {
+		const tableInfo = await queryInterface.describeTable('RssFeedItems');
+		if (!tableInfo.order) {
+			await queryInterface.addColumn('RssFeedItems', 'order', {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				defaultValue: 0,
+			});
+
+			await queryInterface.sequelize.query(`
+        WITH ranked AS (
+          SELECT id, ROW_NUMBER() OVER (ORDER BY "pubDate" DESC) - 1 as new_order
+          FROM "RssFeedItems"
+        )
+        UPDATE "RssFeedItems"
+        SET "order" = ranked.new_order
+        FROM ranked
+        WHERE "RssFeedItems".id = ranked.id;
+      `);
+		}
 	},
 
-	down: async (queryInterface, Sequelize) => {
-		await queryInterface.removeColumn('RssFeedItems', 'order');
+	async down(queryInterface, Sequelize) {
+		const tableInfo = await queryInterface.describeTable('RssFeedItems');
+		if (tableInfo.order) {
+			await queryInterface.removeColumn('RssFeedItems', 'order');
+		}
 	},
 };
