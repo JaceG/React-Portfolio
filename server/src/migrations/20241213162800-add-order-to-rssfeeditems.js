f36e600f37718b2e7f3edf0fa585ced233647032
Jace Galloway <jace.galloway@gmail.com>
2024-12-13 18:10:12 -0500

    trying to fix render error once again

--- server/src/migrations/20241213162800-add-order-to-rssfeeditems.js
+++ server/src/migrations/20241213162800-add-order-to-rssfeeditems.js
@@ -1,32 +1,42 @@
 'use strict';
 
 module.exports = {
-	async up(queryInterface, Sequelize) {
-		const tableInfo = await queryInterface.describeTable('RssFeedItems');
-		if (!tableInfo.order) {
-			await queryInterface.addColumn('RssFeedItems', 'order', {
-				type: Sequelize.INTEGER,
-				allowNull: false,
-				defaultValue: 0,
+	up: function (queryInterface, Sequelize) {
+		return queryInterface
+			.describeTable('RssFeedItems')
+			.then(function (tableInfo) {
+				if (!tableInfo.order) {
+					return queryInterface
+						.addColumn('RssFeedItems', 'order', {
+							type: Sequelize.INTEGER,
+							allowNull: false,
+							defaultValue: 0,
+						})
+						.then(function () {
+							return queryInterface.sequelize.query(`
+              WITH ranked AS (
+                SELECT id, ROW_NUMBER() OVER (ORDER BY "pubDate" DESC) - 1 as new_order
+                FROM "RssFeedItems"
+              )
+              UPDATE "RssFeedItems"
+              SET "order" = ranked.new_order
+              FROM ranked
+              WHERE "RssFeedItems".id = ranked.id;
+            `);
+						});
+				}
+				return Promise.resolve();
 			});
-
-			await queryInterface.sequelize.query(`
-        WITH ranked AS (
-          SELECT id, ROW_NUMBER() OVER (ORDER BY "pubDate" DESC) - 1 as new_order
-          FROM "RssFeedItems"
-        )
-        UPDATE "RssFeedItems"
-        SET "order" = ranked.new_order
-        FROM ranked
-        WHERE "RssFeedItems".id = ranked.id;
-      `);
-		}
 	},
 
-	async down(queryInterface, Sequelize) {
-		const tableInfo = await queryInterface.describeTable('RssFeedItems');
-		if (tableInfo.order) {
-			await queryInterface.removeColumn('RssFeedItems', 'order');
-		}
+	down: function (queryInterface, Sequelize) {
+		return queryInterface
+			.describeTable('RssFeedItems')
+			.then(function (tableInfo) {
+				if (tableInfo.order) {
+					return queryInterface.removeColumn('RssFeedItems', 'order');
+				}
+				return Promise.resolve();
+			});
 	},
 };
