c5b5b870b082f7d69ac3a003f9b9ea91d14eb6d8
Jace Galloway <jace.galloway@gmail.com>
2024-12-13 18:14:45 -0500

    trying to fix render error

--- server/src/migrations/20241213162800-add-order-to-rssfeeditems.js
+++ server/src/migrations/20241213162800-add-order-to-rssfeeditems.js
@@ -2,58 +2,50 @@
 
 module.exports = {
 	up: function (queryInterface, Sequelize) {
-		return new Promise(function (resolve, reject) {
-			queryInterface
-				.describeTable('RssFeedItems')
-				.then(function (tableInfo) {
-					if (!tableInfo.order) {
-						queryInterface
-							.addColumn('RssFeedItems', 'order', {
-								type: Sequelize.INTEGER,
-								allowNull: false,
-								defaultValue: 0,
-							})
-							.then(function () {
-								queryInterface.sequelize
-									.query(
-										`
-                WITH ranked AS (
-                  SELECT id, ROW_NUMBER() OVER (ORDER BY "pubDate" DESC) - 1 as new_order
-                  FROM "RssFeedItems"
-                )
-                UPDATE "RssFeedItems"
-                SET "order" = ranked.new_order
-                FROM ranked
-                WHERE "RssFeedItems".id = ranked.id;
-              `
-									)
-									.then(resolve)
-									.catch(reject);
-							})
-							.catch(reject);
-					} else {
-						resolve();
-					}
-				})
-				.catch(reject);
-		});
+		function handleError(err) {
+			console.error('Migration error:', err);
+			throw err;
+		}
+
+		function updateOrder() {
+			return queryInterface.sequelize.query(
+				'WITH ranked AS (SELECT id, ROW_NUMBER() OVER (ORDER BY "pubDate" DESC) - 1 as new_order FROM "RssFeedItems") UPDATE "RssFeedItems" SET "order" = ranked.new_order FROM ranked WHERE "RssFeedItems".id = ranked.id;'
+			);
+		}
+
+		function addOrderColumn() {
+			return queryInterface.addColumn('RssFeedItems', 'order', {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				defaultValue: 0,
+			});
+		}
+
+		return queryInterface
+			.describeTable('RssFeedItems')
+			.then(function (tableInfo) {
+				if (!tableInfo.order) {
+					return addOrderColumn()
+						.then(updateOrder)
+						.catch(handleError);
+				}
+			})
+			.catch(handleError);
 	},
 
-	down: function (queryInterface, Sequelize) {
-		return new Promise(function (resolve, reject) {
-			queryInterface
-				.describeTable('RssFeedItems')
-				.then(function (tableInfo) {
-					if (tableInfo.order) {
-						queryInterface
-							.removeColumn('RssFeedItems', 'order')
-							.then(resolve)
-							.catch(reject);
-					} else {
-						resolve();
-					}
-				})
-				.catch(reject);
-		});
+	down: function (queryInterface) {
+		function handleError(err) {
+			console.error('Migration error:', err);
+			throw err;
+		}
+
+		return queryInterface
+			.describeTable('RssFeedItems')
+			.then(function (tableInfo) {
+				if (tableInfo.order) {
+					return queryInterface.removeColumn('RssFeedItems', 'order');
+				}
+			})
+			.catch(handleError);
 	},
 };
